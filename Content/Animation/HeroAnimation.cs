e448aced5dccf3da26170b3a0f3b891308ad2fb4
Bert Van Itterbeeck <bvanitterbeeck@gmail.com>
2021-11-26 20:16:51 +0100

    Created Hero Animation Class

---
+++ Content/Animation/HeroAnimation.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_take_2.Content.Animation
+{
+    public class HeroAnimation
+    {
+        private readonly int frameworkwidth = 250;
+        private readonly int frameworkHeight = 200;
+        private readonly int frameworkX = 300;
+        private readonly int frameworkXb = 1133;
+        private readonly AnimationClass walkAnimation;
+        private readonly AnimationClass idleAnimation;
+        private readonly AnimationClass jumpAnimation;
+        private readonly Rectangle[] death = new Rectangle[10];
+        public AnimationClass Walk { get { return walkAnimation; } }
+        public AnimationClass Idle { get { return idleAnimation; } }
+        public AnimationClass Jump { get { return jumpAnimation; } }
+        public Rectangle[] Death { get { return death; } }
+        public HeroAnimation()
+        {
+            #region Walk
+            walkAnimation = new AnimationClass();
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 50, frameworkwidth, frameworkHeight)));
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 50, frameworkwidth, frameworkHeight)));
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 342, frameworkwidth, frameworkHeight)));
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 342, frameworkwidth, frameworkHeight)));
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 634, frameworkwidth, frameworkHeight)));
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 634, frameworkwidth, frameworkHeight)));
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 926, frameworkwidth, frameworkHeight)));
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 926, frameworkwidth, frameworkHeight)));
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 1218, frameworkwidth, frameworkHeight)));
+            walkAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 1218, frameworkwidth, frameworkHeight)));
+            #endregion
+
+            #region Idle
+            idleAnimation = new AnimationClass();
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 50, frameworkwidth, frameworkHeight)));
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 50, frameworkwidth, frameworkHeight)));
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 342, frameworkwidth, frameworkHeight)));
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 342, frameworkwidth, frameworkHeight)));
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 634, frameworkwidth, frameworkHeight)));
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 634, frameworkwidth, frameworkHeight)));
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 926, frameworkwidth, frameworkHeight)));
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 926, frameworkwidth, frameworkHeight)));
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 1218, frameworkwidth, frameworkHeight)));
+            idleAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 1218, frameworkwidth, frameworkHeight)));
+            #endregion
+
+            #region Jump
+            jumpAnimation = new AnimationClass();
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 0, frameworkwidth, frameworkHeight + 65)));
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 0, frameworkwidth, frameworkHeight + 65)));
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 292, frameworkwidth, frameworkHeight + 65)));
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 292, frameworkwidth, frameworkHeight + 65)));
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 584, frameworkwidth, frameworkHeight + 65)));
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 584, frameworkwidth, frameworkHeight + 65)));
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 876, frameworkwidth, frameworkHeight + 65)));
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 876, frameworkwidth, frameworkHeight + 65)));
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 1168, frameworkwidth, frameworkHeight + 65)));
+            jumpAnimation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 1168, frameworkwidth, frameworkHeight + 65)));
+            #endregion
+
+            #region Death
+            death[0] = new Rectangle(frameworkX, 50, frameworkwidth + 25, frameworkHeight + 10);
+            death[1] = new Rectangle(frameworkXb, 50, frameworkwidth + 25, frameworkHeight + 10);
+            death[2] = new Rectangle(frameworkX, 342, frameworkwidth + 25, frameworkHeight + 10);
+            death[3] = new Rectangle(frameworkXb, 342, frameworkwidth + 25, frameworkHeight + 10);
+            death[4] = new Rectangle(frameworkX, 634, frameworkwidth + 25, frameworkHeight + 10);
+            death[5] = new Rectangle(frameworkXb, 634, frameworkwidth + 25, frameworkHeight + 10);
+            death[6] = new Rectangle(frameworkX, 926, frameworkwidth + 25, frameworkHeight + 10);
+            death[7] = new Rectangle(frameworkXb, 926, frameworkwidth + 25, frameworkHeight + 10);
+            death[8] = new Rectangle(frameworkX, 1218, frameworkwidth + 25, frameworkHeight + 10);
+            death[9] = new Rectangle(frameworkXb, 1218, frameworkwidth + 25, frameworkHeight + 10); 
+            #endregion
+        }
+    }
+}
--- Content/Hero/Character.cs
+++ Content/Hero/Character.cs
@@ -18,10 +18,7 @@ namespace project_take_2.Content.Hero
         private Texture2D _textureIdle;
         private Texture2D _textureDie;
         private Texture2D _textureJump;
-        private readonly AnimationClass animation;
-        private readonly AnimationClass animationIdle;
-        private readonly AnimationClass animationJump;
-        private readonly Rectangle[] array = new Rectangle[10];
+        private HeroAnimation _heroAnimation;
         private RectangleF hitbox;
         public static RectangleF positionAndSize;
         public static RectangleF velocity;
@@ -30,10 +27,6 @@ namespace project_take_2.Content.Hero
         private int counter = 0;
         private int counter2 = 1;
         private readonly int OffsetX = 25;
-        private readonly int frameworkwidth = 250;
-        private readonly int frameworkHeight = 200;
-        private readonly int frameworkX = 300;
-        private readonly int frameworkXb = 1133;
         private readonly int _width;
         private readonly int _height;
         private readonly int _x;
@@ -52,53 +45,9 @@ namespace project_take_2.Content.Hero
             _height = height;
             _x = x;
             _y = y;
-
-            animation = new AnimationClass();
             positionAndSize = new RectangleF(_x, _y, _width, _height);
             hitbox = new Rectangle(0, 0, _width, _height);
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 50, frameworkwidth, frameworkHeight)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 50, frameworkwidth, frameworkHeight)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 342, frameworkwidth, frameworkHeight)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 342, frameworkwidth, frameworkHeight)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 634, frameworkwidth, frameworkHeight)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 634, frameworkwidth, frameworkHeight)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 926, frameworkwidth, frameworkHeight)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 926, frameworkwidth, frameworkHeight)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 1218, frameworkwidth, frameworkHeight)));
-            animation.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 1218, frameworkwidth, frameworkHeight)));
-            animationIdle = new AnimationClass();
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 50, frameworkwidth, frameworkHeight)));
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 50, frameworkwidth, frameworkHeight)));
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 342, frameworkwidth, frameworkHeight)));
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 342, frameworkwidth, frameworkHeight)));
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 634, frameworkwidth, frameworkHeight)));
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 634, frameworkwidth, frameworkHeight)));
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 926, frameworkwidth, frameworkHeight)));
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 926, frameworkwidth, frameworkHeight)));
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 1218, frameworkwidth, frameworkHeight)));
-            animationIdle.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 1218, frameworkwidth, frameworkHeight)));
-            animationJump = new AnimationClass();
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 0, frameworkwidth, frameworkHeight + 65)));
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 0, frameworkwidth, frameworkHeight + 65)));
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 292, frameworkwidth, frameworkHeight + 65)));
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 292, frameworkwidth, frameworkHeight + 65)));
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 584, frameworkwidth, frameworkHeight + 65)));
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 584, frameworkwidth, frameworkHeight + 65)));
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 876, frameworkwidth, frameworkHeight + 65)));
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 876, frameworkwidth, frameworkHeight + 65)));
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkX, 1168, frameworkwidth, frameworkHeight + 65)));
-            animationJump.AddFrame(new AnimationFrame(new Rectangle(frameworkXb, 1168, frameworkwidth, frameworkHeight + 65)));
-
-            array[0] = new Rectangle(frameworkX, 50, frameworkwidth+25, frameworkHeight+10);
-            array[1] = new Rectangle(frameworkXb, 50, frameworkwidth+25, frameworkHeight+10);
-            array[2] = new Rectangle(frameworkX, 342, frameworkwidth+25, frameworkHeight+10);
-            array[3] = new Rectangle(frameworkXb, 342, frameworkwidth+25, frameworkHeight+10);
-            array[4] = new Rectangle(frameworkX, 634, frameworkwidth+25, frameworkHeight+10);
-            array[5] = new Rectangle(frameworkXb, 634, frameworkwidth+25, frameworkHeight+10);
-            array[6] = new Rectangle(frameworkX, 926, frameworkwidth+25, frameworkHeight+10);
-            array[7] = new Rectangle(frameworkXb, 926, frameworkwidth+25, frameworkHeight+10);
-            array[8] = new Rectangle(frameworkX, 1218, frameworkwidth+25, frameworkHeight+10);
-            array[9] = new Rectangle(frameworkXb, 1218, frameworkwidth+25, frameworkHeight+10);
+            _heroAnimation = new HeroAnimation();
         }
         #endregion region
 
@@ -135,13 +84,12 @@ namespace project_take_2.Content.Hero
                 DieDraw(_spriteBatch);
             }
         }
-
         private void MoveDraw(SpriteBatch _spriteBatch)
         {
             _spriteBatch.Draw(
                 _texture,
                 (Rectangle)positionAndSize,
-                animation.currentFrame.Source,
+                _heroAnimation.Walk.currentFrame.Source,
                 Color.White,
                 0,
                 new Vector2(0, 0),
@@ -153,7 +101,7 @@ namespace project_take_2.Content.Hero
             _spriteBatch.Draw(
                 _textureJump,
                 (Rectangle)positionAndSize,
-                animationJump.currentFrame.Source,
+                _heroAnimation.Jump.currentFrame.Source,
                 Color.White,
                 0,
                 new Vector2(0, 0),
@@ -165,7 +113,7 @@ namespace project_take_2.Content.Hero
             _spriteBatch.Draw(
                 _textureIdle,
                 (Rectangle)positionAndSize,
-                animationIdle.currentFrame.Source,
+                _heroAnimation.Idle.currentFrame.Source,
                 Color.White,
                 0,
                 new Vector2(0, 0),
@@ -184,7 +132,7 @@ namespace project_take_2.Content.Hero
             _spriteBatch.Draw(
                 _textureDie,
                 (Rectangle)positionAndSize,
-                array[counter],
+                _heroAnimation.Death[counter],
                 Color.White,
                 0,
                 new Vector2(0, 0),
@@ -203,26 +151,25 @@ namespace project_take_2.Content.Hero
             if (Keyboard.GetState().IsKeyDown(Keys.Left) && live )
             {
                 flip = SpriteEffects.FlipHorizontally;
-                animation.Update(gameTime, 10);
+                _heroAnimation.Walk.Update(gameTime, 10);
             }
             if (Keyboard.GetState().IsKeyDown(Keys.Right) && live )
             {
                 flip = SpriteEffects.None;
-                animation.Update(gameTime, 10);
+                _heroAnimation.Walk.Update(gameTime, 10);
             }
             if (Keyboard.GetState().IsKeyUp(Keys.Left) && Keyboard.GetState().IsKeyUp(Keys.Right) && Keyboard.GetState().IsKeyUp(Keys.Up) && Keyboard.GetState().IsKeyUp(Keys.Down)&& live && hasJumped == false )
             {
-                animationIdle.Update(gameTime, 6);
+                _heroAnimation.Idle.Update(gameTime, 6);
             }
             if (hitbox.Intersects(Bear.hitbox) || hitbox.Intersects(Wolf.hitbox)|| hitbox.Intersects(Eagle.hitbox) ||!live)
             {
                 live = false;
             }
-
             // source = Youtube User == Oyyou 
             if(hasJumped == true)
             {                
-                animationJump.Update(gameTime, 6);
+                _heroAnimation.Jump.Update(gameTime, 6);
             }
             if (hasJumped == false)
             {
