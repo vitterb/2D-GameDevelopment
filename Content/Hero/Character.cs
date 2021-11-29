2991cb0782a927b716235f8b44b3151be38c53ad
Bert <bvanitterbeeck@gmail.com>
2021-11-29 08:11:10 +0100

    Character Class cleaned

--- Content/Hero/Character.cs
+++ Content/Hero/Character.cs
@@ -13,31 +13,39 @@ namespace project_take_2.Content.Hero
     public class Character : IHero
     {
         #region variables 
-
-        private Texture2D _texture;
-        private Texture2D _textureIdle;
-        private Texture2D _textureDie;
-        private Texture2D _textureJump;
-        private HeroAnimation _heroAnimation;
-        private RectangleF hitbox;
-        public static RectangleF positionAndSize;
-        public static RectangleF velocity;
-        public static bool live = true;
-        public static bool hasJumped = false;
-        private int counter = 0;
-        private int counter2 = 1;
-        private readonly int OffsetX = 25;
-        private readonly int _width;
-        private readonly int _height;
-        private readonly int _x;
-        private readonly int _y;
-        private SpriteEffects flip = SpriteEffects.None;
-        private SpriteFont font;
-        private Vector2 middle;
-        private string gameOver = "GAME OVER";
-
+        private Texture2D 
+            _texture,
+            _textureIdle,
+            _textureDie,
+            _textureJump;
+        private HeroAnimation 
+            _heroAnimation;
+        private RectangleF 
+            hitbox;
+        public static RectangleF 
+            positionAndSize,
+            velocity;
+        public static bool 
+            live = true,
+            hasJumped = false;
+        private int
+            counter = 0,
+            counter2 = 1;
+        private readonly int 
+            OffsetX = 25,
+            _width,
+            _height,
+            _x,
+            _y;
+        private SpriteEffects 
+            flip = SpriteEffects.None;
+        private SpriteFont 
+            font;
+        private Vector2 
+            middle;
+        private string 
+            gameOver = "GAME OVER";
         #endregion
-
         #region Constructor
         public Character( int x, int y, int width, int height)
         {   
@@ -50,7 +58,6 @@ namespace project_take_2.Content.Hero
             _heroAnimation = new HeroAnimation();
         }
         #endregion region
-
         #region Methodes
         public void LoadContent(ContentManager Content)
         {
@@ -121,14 +128,7 @@ namespace project_take_2.Content.Hero
                 0);
         }
         private void DieDraw(SpriteBatch _spriteBatch)
-        {
-            // TODO naar update
-            counter2++;
-            counter++;
-            if (counter2 > 10)
-            {
-                counter = 9;
-            }
+        {            
             _spriteBatch.Draw(
                 _textureDie,
                 (Rectangle)positionAndSize,
@@ -144,10 +144,14 @@ namespace project_take_2.Content.Hero
         public void update(GameTime gameTime)
         {
             hitboxUpdate();
-
+            counter2++;
+            counter++;
+            if (counter2 > 10)
+            {
+                counter = 9;
+            }
             positionAndSize.X += velocity.X;
             positionAndSize.Y += velocity.Y;
-
             if (Keyboard.GetState().IsKeyDown(Keys.Left) && live )
             {
                 flip = SpriteEffects.FlipHorizontally;
@@ -159,27 +163,17 @@ namespace project_take_2.Content.Hero
                 _heroAnimation.Walk.Update(gameTime, 10);
             }
             if (Keyboard.GetState().IsKeyUp(Keys.Left) && Keyboard.GetState().IsKeyUp(Keys.Right) && Keyboard.GetState().IsKeyUp(Keys.Up) && Keyboard.GetState().IsKeyUp(Keys.Down)&& live && hasJumped == false )
-            {
                 _heroAnimation.Idle.Update(gameTime, 6);
-            }
             if (hitbox.Intersects(Bear.hitbox) || hitbox.Intersects(Wolf.hitbox)|| hitbox.Intersects(Eagle.hitbox) ||!live)
-            {
                 live = false;
-            }
             // source = Youtube User == Oyyou 
             if(hasJumped == true)
-            {                
                 _heroAnimation.Jump.Update(gameTime, 6);
-            }
             if (hasJumped == false)
-            {
                 velocity.Y = 0f;
-            }
             if (positionAndSize.Y + positionAndSize.Height >= 850)
-            {
                 hasJumped = false;
             }
-        }
         private void hitboxUpdate()
         {
             hitbox.X = positionAndSize.X + OffsetX;
