886906fd269c019ba7ae54ead160ff9aa2fb9c71
Bert <bvanitterbeeck@gmail.com>
2021-11-29 08:35:45 +0100

    Cleanup startmenu

--- Content/GameState/Start.cs
+++ Content/GameState/Start.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -11,30 +8,49 @@ namespace project_take_2.Content.GameState
     public class Start
     {
         #region variables
-        private Texture2D StartscreenTexture;
-        private Texture2D buttonStartLevel1;
-        private Texture2D buttonStartLevel2;
-        private Rectangle positionButton1;
-        private Rectangle positionButton2;
-        private Vector2 lvl1;
-        private Vector2 lvl2;
-        private Vector2 StartScreenVector;
-        private string lv1 = "Level1";
-        private string lv2 = "Level2";
-        private SpriteFont font;
-        private bool start = true;
-        private bool start1 = false;
-        private bool start2 = false;
-        private MouseState mouseState;
-        private Color color1;
-        private Color color2;
+        private Texture2D 
+            StartscreenTexture,
+            buttonStartLevel1,
+            buttonStartLevel2;
+        private Rectangle 
+            positionButton1,
+            positionButton2;
+        private Vector2 
+            lvl1,
+            lvl2,
+            StartScreenVector;
+        private string 
+            lv1 = "Level1",
+            lv2 = "Level2";
+        private SpriteFont 
+            font;
+        private bool
+            menu = true,
+            button1 = false,
+            button2 = false;
+        private MouseState 
+            mouseState;
+        private Color 
+            color1,
+            color2;
 
         #endregion
-        #region propo
-        public bool Startmenu { get { return start; } set { start = value; } }
-        public  bool Start1 {get { return start1; } set { start1 = value; }}
-
-        public bool Start2 { get { return start2; } }
+        #region properties
+        public bool Startmenu { get { return menu; } set { menu = value; } }
+        public  bool Start1 {get { return button1; } set { button1 = value; }}
+        public bool Start2 { get { return button2; } }
+        #endregion
+        #region Constructor
+        public Start()
+        {
+            positionButton1 = new Rectangle(300, 200, 200, 200);
+            positionButton2 = new Rectangle(940, 200, 200, 200);
+            lvl1 = new Vector2(300, 450);
+            lvl2 = new Vector2(940, 450);
+            StartScreenVector = new Vector2(0, 0);
+            color1 = Color.White;
+            color2 = Color.White;
+        }
         #endregion
         #region methodes
         public void LoadContent(ContentManager content)
@@ -42,14 +58,7 @@ namespace project_take_2.Content.GameState
             StartscreenTexture = content.Load<Texture2D>("BackGrounds/Cartoon_Forest_BG_01");
             buttonStartLevel1 = content.Load<Texture2D>("buttons/map");
             buttonStartLevel2 = content.Load<Texture2D>("buttons/bag");
-            positionButton1 = new Rectangle(300, 200, 200, 200);
-            positionButton2 = new Rectangle(940,200,200,200);
-            lvl1 = new Vector2(300, 450);
-            lvl2 = new Vector2(940, 450);
-            StartScreenVector = new Vector2(0, 0);
             font = content.Load<SpriteFont>("Font/game");
-            color1 = Color.White;
-            color2 = Color.White;   
         }
         public void Draw(SpriteBatch _spritebatch) 
         {
@@ -67,19 +76,18 @@ namespace project_take_2.Content.GameState
             if (mouseRect.Intersects(positionButton1))
             {
                 color1 = Color.Gold;
-                
                 if (mouseState.LeftButton == ButtonState.Pressed)
                 {
-                    start = false;
-                    start1 = true;
+                    menu = false;
+                    button1 = true;
                 }
             }
-            else { color1 = Color.White; }
+            else 
+                color1 = Color.White; 
             if (mouseRect.Intersects(positionButton2))
-            {
                 color2 = Color.Gold;
-            }
-            else { color2 = Color.White; }
+            else 
+                color2 = Color.White; 
         }
         #endregion
     }
